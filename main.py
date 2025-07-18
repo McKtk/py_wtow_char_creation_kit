707de4bbdd18dd5cfeaf6d0808f976497f9e689d
Maciej Kotkowski <maciej.kotkowski@flokk.com>
2025-07-18 14:03:50 +0200

    first binary file

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 from tkinter import *
 
-#Dic
+def encode(x):
+    return (' '.join(format(ch, 'b') for ch in bytearray(x, encoding='utf8')))
+
+def decode(x):
+    return (''.join(chr(int(b, 2)) for b in x.split()))
+
+def Read_origins():
+    Origin = []
+    with open("./origin.bin", "rb") as f:
+        lines = f.read().decode('utf-8')
+        decoded = decode(lines)
+        print(decoded)
+        for line in decoded.splitlines():
+            Origin.append(line)
+            
+    return Origin
+
+print(Read_origins())
